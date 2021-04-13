ae4efbed53043cb00394b888484c47e4ba276f3d
Casper <casper.aakviststeengaard@gmail.com>
2021-04-13 10:29:06 +0200

    Tilføjet programMenu

---
+++ src/Pizza Hub/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //New Instance af programMenu class
+        ProgramMenu mainMenu = new ProgramMenu();
+        mainMenu.presentMainMenu();
+        boolean endProgram = false;
+
+        while (!endProgram) { //A while loop with a switch to run the menu's and methods
+            switch (mainMenu.fetchUserInput()) {
+                //Add new Order
+                case "1" -> System.out.println("Her skal opret ordre være");
+                //Show preparation order
+                case "2" -> System.out.println("Her skal tilberednings ordre");
+                //Show statistic
+                case "3" -> System.out.println("Her skal statestik være");
+                //To exit HandBook
+                case "9" -> {
+                    System.out.println("Afslutter program");
+                    endProgram = true;
+                }
+                default -> {
+                    System.out.println("Forstår dig ikke. Prøv igen!");
+                    mainMenu.presentMainMenu();
+                }
+            }
+        }
+    }
+}
--- src/Pizza Hub/ProgramMenu.java
+++ src/Pizza Hub/ProgramMenu.java
@@ -4,16 +4,17 @@ public class ProgramMenu {
     private Scanner menuInput = new Scanner(System.in);
 
     public void presentMainMenu(){
-        System.out.println("Welcome to Mario's Pissabar");
+        System.out.println("Velkommen til Mario's Pissabar");
         System.out.println("Tryk 1: Opret ny ordre");
         System.out.println("Tryk 2: Tilberednings rækkefølge");
-        System.out.println("Tryk 3: Check statistik");
+        System.out.println("Tryk 3: Se statistik");
+        System.out.println("Tryk 9: Afslut program");
     }
 
 
     //Scanner class asking for user input
-    public int fetchUserInput() {
-        int userInput = menuInput.nextInt();
+    public  String fetchUserInput() {
+        String userInput = menuInput.nextLine();
         return userInput;
     }
 }
