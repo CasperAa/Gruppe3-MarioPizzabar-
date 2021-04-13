41526eb37c5831c67835a35b8755165aaef35c4d
ahmadasheer <ahmadasheer@hotmail.com>
2021-04-13 11:23:59 +0200

    Tilføjet classes og attributer

--- src/Pizza Hub/Pizza.java
+++ src/Pizza Hub/Pizza.java
@@ -26,5 +26,8 @@ public class Pizza {
     Pizza pizzaTo = new Pizza(2,"Amerikaner", 53,"kød", new String[]{"tomatsauce", "ost", "oksefars", "oregano"});
     Pizza pizzaTre = new Pizza(3,"Cacciatore", 57,"kød", new String[]{"tomatsauce", "ost", "pepperoni", "oregano"});
     Pizza pizzaFire = new Pizza(4,"Carbona", 63,"kød", new String[]{"tomatsauce", "ost", "kødsauce", "spaghetti", "cocktailpølser", "oregano"});
+    Pizza pizzaFem = new Pizza(5,"Dennis",65,"kød", new String[]{"tomatsauce", "ost", "skinke", "pepperoni", "cocktailpølse", "oregano"});
+    Pizza pizzaSeks = new Pizza(6, "Bertil",57,"kød",new String []{"tomatsause", "ost", "bacon", "pepperoni"});
 
 }
+
