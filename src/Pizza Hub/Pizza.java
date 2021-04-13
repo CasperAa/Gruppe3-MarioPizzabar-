442c06f22ca8a1eba440c38b0c3f303cec23822a
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2021-04-13 11:09:13 +0200

    Amanda

--- src/Pizza Hub/Pizza.java
+++ src/Pizza Hub/Pizza.java
@@ -1,14 +1,33 @@
 public class Pizza {
 
-    int nummmer;
+    int nummer;
     String navn;
     int pris;
     String kategori;
     String dejtype;
-    String topping;
+    String[] topping;
     String størrelse;
     String kommentar;
     int ekstraGebyr;
     int antal;
+
+    //Constructor
+    public Pizza(int nummer, String navn, int pris, String kategori, String[] topping) {
+        this.nummer = nummer;
+        this.navn = navn;
+        this.pris = nummer;
+        this.kategori = kategori;
+        this.topping = topping;
+
+    }
+
+    Pizza pizzaEt = new Pizza(1,"Vesuvio", 57,"kød", new String[]{"tomatsauce", "ost", "skinke", "oregano"});
+    Pizza pizzaTo = new Pizza(2,"Amerikaner", 53,"kød", new String[]{"tomatsauce", "ost", "oksefars", "oregano"});
+    Pizza pizzaTre = new Pizza(3,"Cacciatore", 57,"kød", new String[]{"tomatsauce", "ost", "pepperoni", "oregano"});
+    Pizza pizzaFire = new Pizza(4,"Carbona", 63,"kød", new String[]{"tomatsauce", "ost", "kødsauce", "spaghetti", "cocktailpølser", "oregano"});
+
+
+
+
 
 }
