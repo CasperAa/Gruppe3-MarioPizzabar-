dfad95dc9560ffd1bc782b9a94e48cbfe599f23b
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2021-04-13 11:13:07 +0200

    Amanda

--- src/Pizza Hub/Pizza.java
+++ src/Pizza Hub/Pizza.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Pizza {
 
     int nummer;
@@ -18,16 +20,11 @@ public class Pizza {
         this.pris = nummer;
         this.kategori = kategori;
         this.topping = topping;
-
     }
 
     Pizza pizzaEt = new Pizza(1,"Vesuvio", 57,"kød", new String[]{"tomatsauce", "ost", "skinke", "oregano"});
     Pizza pizzaTo = new Pizza(2,"Amerikaner", 53,"kød", new String[]{"tomatsauce", "ost", "oksefars", "oregano"});
     Pizza pizzaTre = new Pizza(3,"Cacciatore", 57,"kød", new String[]{"tomatsauce", "ost", "pepperoni", "oregano"});
     Pizza pizzaFire = new Pizza(4,"Carbona", 63,"kød", new String[]{"tomatsauce", "ost", "kødsauce", "spaghetti", "cocktailpølser", "oregano"});
-
-
-
-
 
 }
