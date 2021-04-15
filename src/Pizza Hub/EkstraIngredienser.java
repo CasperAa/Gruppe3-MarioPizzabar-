90e55295a3db3bd8b29d9fb2b6817e8b226082b7
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2021-04-15 11:33:53 +0200

    Amanda

--- src/Pizza Hub/EkstraIngredienser.java
+++ src/Pizza Hub/EkstraIngredienser.java
@@ -58,15 +58,17 @@ public class EkstraIngredienser {
             }
         }
     }
-
+/*
     //Denne kode skal indsættes der, hvor ingredienser tilføjes
     int endeligPrisPizza;
     if (pizzaMenu.get(ønsketPizza-1).getKategori().equals(Traditional) || pizzaMenu.get(ønsketPizza-1).getKategori().equals(Biache) || pizzaMenu.get(ønsketPizza-1).getKategori().equals(Vegetale)){
         endeligPris = familiePris + ekstraIngredienser;
     }
-    if (pizzaMenu.get(ønsketPizza-1).getKategori().equals(Indbagt) || pizzaMenu.get(ønsketPizza-1).getKategori().equals(Biache) || pizzaMenu.get(ønsketPizza-1).getKategori().equals(Vegetale)){
-        endeligPris = familiePris + ekstraIngredienser;
+    if (pizzaMenu.get(ønsketPizza-1).getKategori().equals(Indbagt) || pizzaMenu.get(ønsketPizza-1).getKategori().equals(Sandwich)){
+        endeligPris = normalPris + ekstraIngredienser;
     }
+
+ */
 
 
 
