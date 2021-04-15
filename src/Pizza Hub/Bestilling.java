0c2161fe8185d00b610cf3e21a0cb5b00a5c0ec8
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2021-04-15 16:31:38 +0200

    Amanda

--- src/Pizza Hub/Bestilling.java
+++ src/Pizza Hub/Bestilling.java
@@ -47,6 +47,7 @@ public class Bestilling {
                 tempPizza = pizzaMenu.get(userPizzaInt-1); //En midlertidig pizza oprettes, så denne kan ændres
                 //pizzaOrder.add(pizzaMenu.get(userPizzaInt-1)); //Denne kode skal ikke bruges i det nye system
                 EkstraIngredienser.familiePizza(userPizzaInt);
+                pizzaOrder.add(tempPizza);
                 System.out.println("Pizza " + userPizzaInt + " er blevet tilføjet");
                 System.out.println("\nIndtast nummer 1-" + pizzaMenu.size() + " for at tilføje.");
             } else if (userPizza.toLowerCase().equals("print") && !pizzaOrder.isEmpty()) {
