3e1842ec57487431fbb6361bf11a9881fd99ee1d
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2021-04-22 14:38:15 +0200

    Amanda

--- src/Pizza Hub/Main.java
+++ src/Pizza Hub/Main.java
@@ -15,38 +15,38 @@ public class Main {
         while (!endProgram) { //while for at køre menuen
             switch (menu.fetchUserInput()) {
 
-                case "1":                //vis pizzamenuen
+                case "1":                //Vis pizzamenuen
                     Pizza.printPizzaMenu();
                     menu.presentMainMenu();
                     break;
 
-                case "2":               //opret ny ordre
+                case "2":               //Opret ny ordre
                     Bestilling.opretOrdre();
                     menu.presentMainMenu();
                     break;
 
-                case "3":                //vis forberedelsesrækkefølgen
+                case "3":                //Vis forberedelsesrækkefølgen
                     Bestilling.sorterListe(Bestilling.alleOrdrer);
                     Bestilling.printTilberedningsRækkefølge(Bestilling.alleOrdrer);
                     System.out.println("Du er i hovedmenuen nu!");
                     break;
 
-                case "4":                //vis statistik
+                case "4":                //Vis statistik
                     Statistik.statistikMenu();
                     System.out.println("Du er i hovedmenuen nu!");
                     break;
 
-                case "5":                //fjern en ordre fra ordrelisten
+                case "5":                //Fjern en ordre fra ordrelisten
                     Bestilling.sletOrdre();
                     menu.presentMainMenu();
                     break;
 
-                case "9":                //exit program
+                case "9":                //Exit program
                     System.out.println("Afslutter program.");
                     endProgram = true;
                     break;
 
-                default:                //hvis brugeren skriver noget, programmet ikke forstår
+                default:                //Hvis brugeren skriver noget, programmet ikke forstår
                     System.out.println("Jeg forstår dig ikke. Prøv igen!");
                     menu.presentMainMenu();
             }
